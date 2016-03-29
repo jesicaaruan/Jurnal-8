82984c0782a461d16939c64cd184f12ce9f4f655
Jesica Vetra Aruan <jesicavetra@gmail.com>
2016-03-29 15:33:21 +0700

    Added files via upload

---
+++ main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Praktikum
+ */
+
+public class main {
+    public static void main(String[] args) {
+        int deret=Integer.parseInt(JOptionPane.showInputDialog("Masukkan berapa deret Fibonacci: "));
+        int a=0;
+        int b=1;
+        System.out.print(deret+" deret Fibonacci: " );
+        for (int i=0;i<deret;i++){
+            System.out.print(a+" ");
+            a=a+b;
+            b=a-b;   
+        }
+    }
+}
+    
+
